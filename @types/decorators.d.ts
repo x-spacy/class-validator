36c209946764c1da4b0a93955e6b71f3e759926f
Vinícius Gutierrez <srgutyerrez@gmail.com>
2025-08-12 22:58:24 -0300

    chore: try to export directly on decorators

--- @types/decorators.d.ts
+++ @types/decorators.d.ts
@@ -2,5 +2,8 @@ declare module '@x-spacy/validators' {
   export type IsDocumentOptions = {
     type: DocumentTypeEnum;
   };
+
   export declare function IsDocument(isDocumentOptions: IsDocumentOptions, validationOptions?: ValidationOptions): PropertyDecorator;
+
+  export * from 'class-validator';
 }
--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="decorators.d.ts" />
 /// <reference path="enums.d.ts" />
 /// <reference path="services.d.ts" />
-/// <reference path="../node_modules/class-validator/types/index.d.ts" />
