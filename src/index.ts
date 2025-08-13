c8c48f20bb2b5837a51c153f2b453d4ac475f0c1
Vinícius Gutierrez <srgutyerrez@gmail.com>
2025-08-12 22:59:45 -0300

    lint: apply linter rules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 export * from '@x-spacy/validators/decorators/IsDocument';
+
 export * from '@x-spacy/validators/enums/DocumentTypeEnum';
+
 export * from '@x-spacy/validators/services/IsCNPJ';
+
 export * from '@x-spacy/validators/services/IsCPF';
+
 export * from 'class-validator';
