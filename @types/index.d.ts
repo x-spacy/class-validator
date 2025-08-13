01ac89959d55476db76ede53b2138ddb061ea65d
Vinícius Gutierrez <srgutyerrez@gmail.com>
2025-08-12 22:57:01 -0300

    chore: try another way to export class validator types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="decorators.d.ts" />
 /// <reference path="enums.d.ts" />
 /// <reference path="services.d.ts" />
-
-export * from 'class-validator';
+/// <reference path="../node_modules/class-validator/types/index.d.ts" />
