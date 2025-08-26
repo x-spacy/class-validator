22aa75015ed1fceb7439510ad1f7b13584cdff7e
Vinícius Gutierrez <srgutyerrez@gmail.com>
2025-08-26 13:37:24 -0300

    feat: add more flexibility to get type by object

--- src/decorators/IsDocument.ts
+++ src/decorators/IsDocument.ts
@@ -1,7 +1,9 @@
 import {
   registerDecorator,
   ValidationArguments,
-  ValidationOptions
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface
 } from 'class-validator';
 
 import { isCNPJ } from '@x-spacy/class-validator/services/IsCNPJ';
@@ -12,44 +14,54 @@ import { isCRN } from '@x-spacy/class-validator/services/IsCRN';
 
 import { DocumentTypeEnum } from '@x-spacy/class-validator/enums/DocumentTypeEnum';
 
-export function IsDocument(isDocumentOptions: { type: DocumentTypeEnum }, validationOptions?: ValidationOptions) {
+@ValidatorConstraint({ name: 'IsDocument', async: false })
+export class IsDocumentConstraint implements ValidatorConstraintInterface {
+  validate(document: unknown, args: ValidationArguments) {
+    if (typeof document !== 'string') {
+      return false;
+    }
+
+    const options = args.constraints[0] as { type?: DocumentTypeEnum };
+    const instance = args.object as { type?: DocumentTypeEnum };
+
+    const type = options?.type ?? instance.type;
+
+    switch (type) {
+      case DocumentTypeEnum.CPF: {
+        return isCPF(document);
+      }
+      case DocumentTypeEnum.CNPJ: {
+        return isCNPJ(document);
+      }
+      case DocumentTypeEnum.CRN: {
+        return isCRN(document);
+      }
+      case DocumentTypeEnum.CREF: {
+        return isCREF(document);
+      }
+      case DocumentTypeEnum.CRM: {
+        return isCRM(document);
+      }
+      default: {
+        return false;
+      }
+    }
+  }
+
+  defaultMessage() {
+    return 'Informe um documento válido';
+  }
+}
+
+export function IsDocument(isDocumentOptions: { type?: DocumentTypeEnum } = {}, validationOptions?: ValidationOptions) {
   return function (object: Object, propertyName: string) {
     registerDecorator({
       name: 'IsDocument',
       target: object.constructor,
       propertyName: propertyName,
       options: validationOptions,
-      validator: {
-        validate(document: unknown, _args: ValidationArguments) {
-          if (typeof document !== 'string') {
-            return false;
-          }
-
-          switch (isDocumentOptions.type) {
-            case DocumentTypeEnum.CPF: {
-              return isCPF(document);
-            }
-            case DocumentTypeEnum.CNPJ: {
-              return isCNPJ(document);
-            }
-            case DocumentTypeEnum.CRN: {
-              return isCRN(document);
-            }
-            case DocumentTypeEnum.CREF: {
-              return isCREF(document);
-            }
-            case DocumentTypeEnum.CRM: {
-              return isCRM(document);
-            }
-            default: {
-              return false;
-            }
-          }
-        },
-        defaultMessage() {
-          return 'Informe um documento válido';
-        }
-      }
+      constraints: [ isDocumentOptions ],
+      validator: IsDocumentConstraint
     });
   };
 }
