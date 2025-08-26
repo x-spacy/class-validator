2f57b1351a3659dbee3bae25f8f44a0302ae2f17
Vinícius Gutierrez <srgutyerrez@gmail.com>
2025-08-26 13:07:14 -0300

    fix: missing export cref, crm and crn

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,10 @@ export * from '@x-spacy/class-validator/services/IsCNPJ';
 
 export * from '@x-spacy/class-validator/services/IsCPF';
 
+export * from '@x-spacy/class-validator/services/IsCREF';
+
+export * from '@x-spacy/class-validator/services/IsCRM';
+
+export * from '@x-spacy/class-validator/services/IsCRN';
+
 export * from 'class-validator';
